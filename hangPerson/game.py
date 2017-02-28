2f46d89da6e39cc8eeaa6d29a4d5cda0bb5a2371
Thomas Johnson <tjohnson061944@gmail.com>
2017-02-28 13:18:20 -0600

    Added an additional list to show your progess guessing.

--- hangPerson/game.py
+++ hangPerson/game.py
@@ -2,31 +2,44 @@ import gameParameters
 
 newGame = gameParameters.Parameters()
 newGame.initWord()
-wordList = []
-
-#Generating a list form of the word being guessed
+inputTable = {}
+answerList = []
 for letters in newGame.word:
-    if(letters.isspace()):
-        wordList.append("space")
-    else:
-        wordList.append("")
-tuple(wordList)
+    answerList.append("")
 
 print "For testing purposes, the answer is %s" %newGame.word
 #The main game engine
-while (not newGame.solved and newGame.getGuesses() <= 8):
-    userInput = raw_input("Guess a letter or phrase!\n")
+while (not newGame.solved):
+    print answerList
+    print "So far you have guessed:"
+    for keys in inputTable:
+        print keys,
+    userInput = raw_input("\nGuess a letter or phrase!\n")
     userInput = userInput.lower()
+
+    if(userInput not in inputTable):
+        newLetter = {userInput: 1}
+        inputTable.update(newLetter)
+    else:
+        print "You have already guessed that letter!"
+        continue
 
     if(userInput == newGame.word):
         newGame.solved = True
 
     elif(userInput in newGame.word):
+        print "-" * 25
         print "Correct!"
-
+        for i in range(len(answerList)):
+            if(newGame.word[i] == userInput):
+                answerList[i] = userInput
     else:
-        print "Guess Again!"
         newGame.incGuess()
+        if(newGame.getGuesses() >= 8):
+            break
+        else:
+            print "-" * 25
+            print "Guess Again!"
 
 if(not newGame.solved):
     print "You lose! Better luck next time."
